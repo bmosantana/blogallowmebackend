826506bcb0021701c204be18dd81a464341c5733
Brenda Santana <santanabrenda474@gmail.com>
2024-04-02 18:46:54 -0300

    Criando base de dados e tabelas

---
+++ sql.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE blog_api;
+\c blog_api;
+
+CREATE TABLE  noticias (
+    id SERIAL PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    dt_criacao DATE NOT NULL,
+    texto TEXT NOT NULL,
+    -- autor_id 
+    
+);
