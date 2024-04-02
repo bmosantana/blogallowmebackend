36f3171a6d12f74052a17b03b5cfaaa633072b9b
Brenda Santana <santanabrenda474@gmail.com>
2024-04-02 20:08:51 -0300

    get all noticias - aprendendo também uma nova forma de usar as respostas (res)

--- server.js
+++ server.js
@@ -16,3 +16,31 @@ const pool = new Pool({
     port: 5432,
 });
 
+// analise do corpo da requisição
+app.use(bodyParser.json());
+
+// Rotas
+app.get('/noticias', async (req, res) => {
+    const { titulo, texto, dt_criacao } = req.body;
+
+    try {
+        const client = await pool.connect();
+        const result = await client.query('SELECT * FROM  noticias')
+        const items = result.rows;
+
+        client.release();
+        res.json(items);
+
+    } catch (err) {
+        console.error('Erro ao buscar itens:', err);
+        res.status(500).json({message: 'Erro interno do servidor'})
+    }
+});
+
+app.get('/noticias/:id', async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+});
